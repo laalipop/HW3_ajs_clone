4553f107986b655001bf855ffaa0292fb6129180
laalipop <preicpodhya@mail.ru>
2020-12-25 15:34:25 +0300

    corrected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 function info() {
     const m = new Map();
-    m.set('name', 'ajs');
+    m.set('name', '3hw_ajs');
     m.set('version', '1.0.0');
     return m;
   }
   
-  export default {
-    info
-  };
+  // export default {
+  //   info
+  // };
+
+  const ajs = require('@laalipop/3hw_ajs');
+
+console.log(ajs.info());
